b36961e6ad7fc7ca4fc584c758d5f13981597921
sosdan73 <daniel.sosinskiy@gmail.com>
2023-01-15 18:14:10 +0200

    adds toggle logic to the state

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,6 +6,7 @@ import {useSelector, useDispatch} from "react-redux";
 const Counter = () => {
 	const dispatch = useDispatch()
 	const counter = useSelector(state => state.counter);
+	const showCounter = useSelector(state => state.showCounter)
 
 	const handleIncrement = () => {
 		dispatch({type: 'increment'});
@@ -19,12 +20,14 @@ const Counter = () => {
 		dispatch({type: 'increase', amount: 10});
 	}
 
-	const toggleCounterHandler = () => {};
+	const toggleCounterHandler = () => {
+		dispatch({type: 'toggle'})
+	};
 
 	return (
 		<main className={classes.counter}>
 			<h1>Redux Counter</h1>
-			<div className={classes.value}>{counter}</div>
+			{showCounter && <div className={classes.value}>{counter}</div>}
 			<div>
 				<button onClick={handleIncrement}>Increment</button>
 				<button onClick={handleIncrease}>Increase by 10</button>
