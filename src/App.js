8213065f726eedd3e8c8e5716d7749640ef25664
sosdan73 <daniel.sosinskiy@gmail.com>
2023-01-18 17:01:35 +0200

    adds components for auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import Counter from './components/Counter';
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import {useSelector} from "react-redux";
+import UserProfile from "./components/UserProfile";
 
 
 function App() {
-  return (
-    <Counter />
-  );
+	const isAuth = useSelector(state => state.auth.isAuthenticated);
+
+	return (
+		<>
+			<Header />
+			{!isAuth && <Auth/>}
+			{isAuth && <UserProfile />}
+			<Counter />
+		</>
+	);
 }
 
 export default App;
